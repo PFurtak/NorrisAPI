2bb050eaa92589eb8f152f90dc97261b2b4c040e
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-10 15:21:23 -0500

    added button for animal category

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,13 @@
 'use strict';
+let category = 'dev';
 
-const apiUrl = 'https://api.chucknorris.io/jokes/random?category=dev';
 const refreshQuoteButton = document.querySelector('#refreshQuote');
+const submitFormButton = document.querySelector('#submitForm');
 
-function getQuote() {
+function getQuote(category) {
+  const apiUrl = `https://api.chucknorris.io/jokes/random?category=${category}`;
   const chuckSaysParagraph = document.querySelector('#chuckSays');
+
   get(apiUrl).then(function(response) {
     chuckSaysParagraph.innerHTML = response.value;
   });
@@ -12,7 +15,15 @@ function getQuote() {
 
 refreshQuoteButton.addEventListener('click', function(e) {
   e.preventDefault();
-  getQuote();
+  getQuote(category);
 });
 
-getQuote();
+submitFormButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  const categoryInput = document.querySelector('#categoryInput');
+
+  category = categoryInput.value;
+  getQuote(category);
+});
+
+getQuote(category);
